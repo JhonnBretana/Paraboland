d8e05a560fb37a6f13ebd16ca6bdf987f35f66de
Kurtpantaleon <kurtkurt05pantaleon@gmail.com>
2025-08-12 20:54:59 +0800

    added settings manager

---
+++ Assets/Script/SettingsManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour
+{
+    [Header("References")]
+    public GameObject settingsModal; // Assign your SettingsModal GameObject here
+
+    // Call this from the Settings button OnClick()
+    public void OpenSettings()
+    {
+        if (settingsModal != null)
+            settingsModal.SetActive(true);
+    }
+
+    // Call this from the Close button inside the modal
+    public void CloseSettings()
+    {
+        if (settingsModal != null)
+            settingsModal.SetActive(false);
+    }
+
+    // Call this from the Clear Records button OnClick()
+    public void ClearRecords()
+    {
+        PlayerPrefs.DeleteAll(); // Clears all PlayerPrefs (character selection, answers, etc.)
+        PlayerPrefs.Save();
+        Debug.Log("All records cleared!");
+    }
+
+    // Call this from the Exit App button OnClick()
+    public void ExitApp()
+    {
+        Application.Quit();
+        Debug.Log("Exiting app...");
+    }
+}
